84e503f72241b8aa3b13fb563c4932b81e20d5e1
FelipeFreitasRossi <lipeferossi@gmail.com>
2026-05-05 21:34:05 -0300

    refazendo o push

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    minify: false,      
-    transformer: 'postcss'
+    minify: false,          // desativa o LightningCSS (causa do erro)
+    transformer: 'postcss'  // usa o PostCSS padrão
   }
 })
